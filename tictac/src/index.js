0fdf0e4ec5f1d080eb37d753888838236db84b61
MarkoOtlokan <otlokanmarko@gmail.com>
2020-10-12 13:19:51 +0200

    table

--- tictac/src/index.js
+++ tictac/src/index.js
@@ -1,39 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import SmallBoard from './SmallBoard';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
 
-class Board extends React.Component {
+class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
       xIsNext: true,    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (this.calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({      squares: squares,      xIsNext: !this.state.xIsNext,    });
-  }
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
   }
 
   calculateWinner(squares) {
@@ -66,35 +42,43 @@ class Board extends React.Component {
        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
     return (
+      <div className="game">
+
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+        <div className="rows">
+          <div  className="row">
+            <SmallBoard />
+          </div>
+          <div  className="row">
+            <SmallBoard />
+          </div>
+          <div className="row">
+            <SmallBoard />
+          </div>
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+        <div className=".board-row">
+          <div  className="row">
+            <SmallBoard />
+          </div>
+          <div  className="row">
+            <SmallBoard />
+          </div>
+          <div className="row">
+            <SmallBoard />
+          </div>
         </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+        <div className=".board-row">
+          <div  className="row">
+            <SmallBoard />
+          </div>
+          <div  className="row">
+            <SmallBoard />
+          </div>
+          <div className="row">
+            <SmallBoard />
+          </div>
         </div>
       </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
         <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
