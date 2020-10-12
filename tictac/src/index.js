9edc07b3702a7f706435d6e5580a04baba032f4f
MarkoOtlokan <otlokanmarko@gmail.com>
2020-10-12 11:51:49 +0200

    X O

--- tictac/src/index.js
+++ tictac/src/index.js
@@ -11,20 +11,36 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: this.props.value})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      squares: Array(9).fill(null),
+      player:"X",
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
+  }
+
+  handleClick(i){
+    this.state.squares[i] = this.state.player;
+    this.setState({player: (this.state.player === "X")? "O" : "X"});
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '+this.state.player;
 
     return (
       <div>
